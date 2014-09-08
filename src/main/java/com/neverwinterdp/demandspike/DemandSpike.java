e4b229c3fb5d9103c9fe9b5c84d7a686e94be1e8
Peter Jerold Leslie <leslie@charlietech.com>
2014-09-08 11:37:44 +0530

    new

--- src/main/java/com/neverwinterdp/demandspike/DemandSpike.java
+++ src/main/java/com/neverwinterdp/demandspike/DemandSpike.java
@@ -3,75 +3,40 @@ package com.neverwinterdp.demandspike;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.beust.jcommander.JCommander;
-import com.beust.jcommander.Parameter;
-import com.beust.jcommander.ParameterException;
-
-
+import com.neverwinterdp.demandspike.commandline.DemandSpikeParser;
 
 public class DemandSpike {
-
-	
-	private class demandspikeCommands{
-	    //////////////////////////////////////////////////////////////////////////////////////
-	    //Command line args
-	    @Parameter(names={"run"},description="Run test job")
-	    public boolean run = false;
-	   
-	    @Parameter(names={"-help","--help","-h"}, description="Displays help message")
-	    public boolean help = false;
-	    //////////////////////////////////////////////////////////////////////////////////////
-	  }
-	
-	
 	private static Logger logger;
 	private boolean help;
+	//public boolean run;
+	public boolean stop;
+	public boolean pause;
+	public boolean list;
+	
+	
+	
 	public static void main(String[] args) {
 		try {
-			DemandSpike ds = new DemandSpike(args);
+			
+			//String[] argv = { "run","--target","http://127.0.0.1:7080","--protocol","HTTP","--method","POST","--time","300","--cLevel","2" };
+			
+			DemandSpikeParser demandSpikeParser = new DemandSpikeParser();
+			
+			
+			
+			
+			if (!demandSpikeParser.parseCommandLine(args)) {
+				System.exit(-1);
+			}
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-	public DemandSpike(String args[]) {
+	public DemandSpike() {
 		logger = LoggerFactory.getLogger("DemandSpike");
-		logger.info("Initializing DemandSpike object");
-		for (int i = 0; i < args.length; i++) {
-			logger.info("Args passed in:" + args[i]);
-		}
-		
-		// Exit out if there's a problem parsing out command line args
-		// Or if the help flag was passed in
-		if (!this.parseCommandLine(args) || this.help) {
-			System.exit(-1);
-		}
+
 	}
 	
-	
-	 public boolean parseCommandLine(String[] args){
-		 	demandspikeCommands parser = new demandspikeCommands();
-		    JCommander jcomm=null;
-		    try{
-		      jcomm = new JCommander(parser, args);
-		    }
-		    catch(ParameterException e){
-		      System.err.println(e.getMessage()+"\nUse the -h option to get usage");
-		      return false;
-		    }
-		  
-		    if (args.length < 2) {
-		    	jcomm.usage();
-		        return true;
-		    }
-		    
-		    this.help = parser.help;
-		    
-		    //If -help was on command line,
-		    //Print out help message
-		    if (this.help){
-		      jcomm.usage();
-		    }
-		    return true;
-		  }
 }
